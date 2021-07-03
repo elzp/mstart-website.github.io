0fe6760204007f90529c12009e7c75c401698178
Ela Piech <e-l-a-p@wp.pl>
2021-07-03 09:51:56 +0200

    Add limit for questions and info's for user

--- src/components/Verification/Verification.js
+++ src/components/Verification/Verification.js
@@ -5,20 +5,42 @@ import { getQuestion } from './../../functionsAndVars';
 function Verification(props) {
 
     const [questionData, setQuestionData] =useState(getQuestion([]));//props.firstQuestion);
+    const [numberOfGoodQuestions, setNumberOfGoodQuestions] = useState(0);
+    const [text, settext] = useState('');
 
     const {question: {question, answers, goodAnswer}, askedQuestions} = questionData;
+    const errorText = 'you had 5 chanses and you did\'t get 3 good answers.';
+    const sendText = 'your\'e message was send';
 
     const handleAnswering = (event) => {
+      
+      if(event.target.value === answers[goodAnswer-1]){
+        setNumberOfGoodQuestions(numberOfGoodQuestions+1);
+      }
 
       const newQuestion = getQuestion(askedQuestions);
       const {question:{question: textOfQuestion}} =  newQuestion;
       if(textOfQuestion !==''){
         setQuestionData(newQuestion);
       }
-
+      else{ 
+        if(text !== sendText) settext(errorText);
+       }
     }
 
+    // handling getting 3 good answers
+    useEffect(()=>{
+      if(numberOfGoodQuestions===3 & (text !== errorText)) {
+        settext(sendText);
+        // area to add function to send data from form 
+      }
+    }, [numberOfGoodQuestions])
 
+    // restarting process of verifying
+    useEffect(()=>{
+      if(text == errorText) {
+      }
+    }, [numberOfGoodQuestions])
 
   return (
     <div 
@@ -35,6 +57,8 @@ function Verification(props) {
         value={item}
         >{item}</button>
         </div>)}
+        <div>{text}</div>
+
     </div>
 
   );
