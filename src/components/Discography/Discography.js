f7f219baed842a5fe023b202fb59ebfbc2dec8d9
Ela Piech <e-l-a-p@wp.pl>
2021-07-28 19:22:52 +0200

    Move translation of CD images to component with...
    styled-component library

--- src/components/Discography/Discography.js
+++ src/components/Discography/Discography.js
@@ -1,10 +1,17 @@
 import './../../dist/App.css';
-import React from 'react';
+import React, {useState} from 'react';
 import {infoAboutCds} from '../../functionsAndVars';
+import styled from 'styled-components'
 
+
+const Image = styled.img`
+transform: ${props => props.count ? "translateX(-90%)" : "translateX(0%)"};
+`
 
 function Discography(props) {
   const cdsArray = infoAboutCds;
+  const [count, setcount] =useState(//0);
+  {0:false,1:false,2:false,3:false});
 
   return (
     <div id="discs">
@@ -14,19 +21,28 @@ function Discography(props) {
            cdsArray.map((it, index)=>{
             return (
             <div className="disc-container">
-              <div key={index}
+              <div key={`dev-${index}`}
               className="disc-img">
                 <div>
                 {it[3].split(",").map(it1=>(<p>{it1}</p>))}
                 </div>
-                <img src={it[0]} 
+                <Image key={`img-${index}`} src={it[0]} count={count[index]}
+                onMouseOver={(e) => {setcount(count=> //count +1
+                 { const f = //{...count};
+                  {...count, 
+                    [index]: !count[index],
+                  };
+                  return f;
+                  }
+                  )}}
                 alt= {it[1]}
                 />
               </div>
               <div 
               className="disc-text"
-              key={index}>
+              key={`text-${index}`}>
                 <p className="disc-title">{it[1]}</p>
+                {/* <p  key={`p-${index}`} >{JSON.stringify(count[index])}</p> */}
                 <p className="disc-year">{it[2]}</p>
                 </div>
             </div>
