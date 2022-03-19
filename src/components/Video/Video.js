2643dacf1db290087680f265731b4dae1ecd5e14
Ela Piech <e-l-a-p@wp.pl>
2022-03-19 18:56:40 +0100

    Add ability to change video using keyboard

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,9 +1,11 @@
 import './../../dist/App.css';
 
 function Video (props) {
-
+    const setId = () => props.setIdOfDisplayedVideo(props.id);
     return (
-      <div className="video" onClick={()=>props.setIdOfDisplayedVideo(props.id)}> 
+      <div className="video" onClick={()=>setId()} onkey
+      onKeyPress={(event)=>{if(event.key === "Enter") setId()}}
+      tabIndex="0"> 
         <h6>{props.title}</h6>
         <div>
         <img src={`https://i1.ytimg.com/vi/${props.videoId}/sddefault.jpg`} alt={props.title} />
